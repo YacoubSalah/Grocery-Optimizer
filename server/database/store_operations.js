430523cbd7a484f70c6f71f1303f2b1f032464ab
YacoubSalah <eng.yacoub-salah-89@hotmail.com>
2022-07-06 10:16:27 +0300

    post drop menu filters api are ready

--- server/database/store_operations.js
+++ server/database/store_operations.js
@@ -1,23 +1,56 @@
 const storeModel = require("../models/store")
+const productModel = require("../models/product")
 
 async function getStoresNamesList(filter) {
 
-    let stores = await storeModel.find().exec()
-    stores = stores.map(s => {
-        return ({ storeName: s.name, storeLocation: s.location })
-    })
+    let storesList = []
 
-    let storesNamesList = []
-    return stores
+    if (filter.storeLocationFilter) {
+        storesList = await storeModel.find({ location: filter.storeLocationFilter }).exec()
+    } else {
+        storesList = await storeModel.find({}).exec()
+    }
 
+    let StoresNameList = []
+
+    if (filter.productNameFilter) {
+        for (let store of storesList) {
+            let product = await productModel.findOne({ name: filter.productNameFilter, 'stores.storeId': store.id }).exec()
+            if (product) {
+                StoresNameList.push(store.name)
+            }
+        }
+    } else {
+        StoresNameList = storesList.map(s => s.name)
+    }
+
+    return StoresNameList
 }
 
-async function getStoresLocationsList(filter){
+async function getStoresLocationsList(filter) {
 
-    let storesLocationsList = []
-    return stores
+    let storesList = []
+
+    if (filter.storeNameFilter) {
+        storesList = await storeModel.find({ name: filter.storeNameFilter }).exec()
+    } else {
+        storesList = await storeModel.find({}).exec()
+    }
+
+    let StoresLocationList = []
+
+    if (filter.productNameFilter) {
+        for (let store of storesList) {
+            let product = await productModel.findOne({ name: filter.productNameFilter, 'stores.storeId': store.id }).exec()
+            if (product) {
+                StoresLocationList.push(store.location)
+            }
+        }
+    } else {
+        StoresLocationList = storesList.map(s => s.name)
+    }
+
+    return StoresLocationList
 }
 
-
-
-module.exports = { getStoresNamesList , getStoresLocationsList }
+module.exports = { getStoresNamesList, getStoresLocationsList }
