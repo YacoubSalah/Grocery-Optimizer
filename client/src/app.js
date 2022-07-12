fe93e8355ceea88c5b595fc5aaa81a84c0f731f2
YacoubSalah <eng.yacoub-salah-89@hotmail.com>
2022-07-12 18:44:33 +0300

    more CSS

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,7 @@ import Navbar from './components/navigation_bar/navigation_bar'
 import Home from './components/home/home';
 import ProductsSearch from './components/products_search/products_search'
 import StoreSearch from './components/stores_search/stores_search'
+import NewHome from './components/new-home/new-home'
 
 import './app.css'
 
@@ -16,7 +17,8 @@ function App() {
         </div>
         <div className="appBody">
           <Routes >
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<NewHome />} />
+            {/* <Route path='/' element={<Home />} /> */}
             <Route path='/products' element={<ProductsSearch />} />
             <Route path='/stores' element={<StoreSearch />} />
           </Routes>
--- client/src/components/navigation_bar/navigation_bar.js
+++ client/src/components/navigation_bar/navigation_bar.js
@@ -5,8 +5,12 @@ import './navgiation_bar.css'
 function navigationBar() {
     return (
         <div className="Navbar">
-            <a href="/" className='navBarLogo'><img src="https://i.ibb.co/JkxRMhr/GO-Logo.png"></img></a>
-            <a href="/" className="name">GroceryOptimizer</a>
+            <Link className='logoImage' to="/">
+                <img src="https://i.ibb.co/JkxRMhr/GO-Logo.png"></img>
+            </Link>
+            <Link className="logoText" to="/">
+                <div>GroceryOptimizer</div>
+            </Link>
             <Link className='link' to="/">Home</Link>
             <Link className='link' to="/products">Products</Link>
             <Link className='link' to="/stores">Stores</Link>
