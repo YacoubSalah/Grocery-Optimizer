5639af8226f017383658dc5ad6e61d3a5afa77c8
BasemBw <basem.bwerat11@gmail.com>
2022-07-12 10:21:27 +0300

    working on stores page

--- client/src/components/store/store.jsx
+++ client/src/components/store/store.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import './store.css'
 import { Rating } from 'react-simple-star-rating'
 
-function store() {
+function store(props) {
+
+  console.log(props.store)
   return (
     <div className='storeCard'>
-        <div>
-            <h3>Store Name</h3>
-            <h3>Store City</h3>
-            <h3>total : 10</h3>
-            <button className='detalisButton'>Detalis</button>
-        </div>
-        <div>
-            <img src='https://static.wikia.nocookie.net/phobia/images/8/82/Milk2.jpg/revision/latest/scale-to-width-down/360?cb=20170124115645' alt="imageMilk" />
-            <div className='starsDiv'>
-               <Rating />
-            </div>
-        </div>
+      <div className='imagAndScoreDiv'>
+        <img className='storeImage' src='https://bombyxplm.com/wp-content/uploads/2021/01/421-4213053_default-avatar-icon-hd-png-download.png' alt="defult" />
+        <div><Rating className='ratingSpan' ratingValue={20} /></div>
+      </div>
+      <div className='DetailsDiv'>
+         <h3>{props.store.name}</h3>
+         <h3>{props.store.location}</h3>
+         <h3>Total Price : {props.store.totalPrice}$</h3>
+      </div>
+      <button class="btn">Details</button>
     </div>
   )
 }
