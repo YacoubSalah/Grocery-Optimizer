ee19cc23992d567d48f1e7aa81111032be5e4827
YacoubSalah <eng.yacoub-salah-89@hotmail.com>
2022-07-15 00:43:18 +0300

    solved bug in details

--- client/src/store/carts.js
+++ client/src/store/carts.js
@@ -7,38 +7,67 @@ export class Carts {
 
         this.storesCarts = []
         this.filteredStoresCarts = []
-        this.itemToShow = null
-        this.feedBack = []
-        this.totalPrice = 0
         this.loadingStoresSnackBar = false
-        this.requsetStoresStatus = true
         this.storesNameList = []
         this.citiesNameList = []
         this.cityNameFilter = ""
         this.storeNameFilter = ""
+        
+        //not revised
+        this.itemToShow = null
+
+        this.feedBack = []
+        this.totalPrice = 0
+        this.requsetStoresStatus = true
+        //not revised
 
         makeObservable(this, {
             storesCarts: observable,
             filteredStoresCarts: observable,
-            itemToShow: observable,
-            feedBack: observable,
-            totalPrice: observable,
             loadingStoresSnackBar: observable,
-            requsetStoresStatus: observable,
             citiesNameList: observable,
             storesNameList: observable,
 
+            //not revised
+            itemToShow: observable,
+
+            feedBack: observable,
+            totalPrice: observable,
+            requsetStoresStatus: observable,
+            //not revised
+
             updateStoresCarts: action,
             updatedFilteredStoresCarts: action,
+            handleLoadinStoresSnackBar: action,
+            updateCitiesNameList: action,
+            updateStoresNameList: action,
+
+            //not revised
             addItemToShow: action,
             updateFeedBack: action,
             calculateTotalPrices: action,
-            handleLoadinStoresSnackBar: action,
             UpdateRequestStatus: action,
-            updateCitiesNameList: action,
-            updateStoresNameList: action
+            //not revised
         })
 
+    }
+
+    getStoresByProducts = (cart) => {
+        this.loadingStoresSnackBar = true
+        axios.post(`http://localhost:3020/cartPrices`, { cart })
+            .then((response) => {
+                this.handleLoadinStoresSnackBar()
+                this.updateStoresCarts(response.data)
+                this.updatedFilteredStoresCarts()
+                this.calculateTotalPrices(cart)
+            })
+            .catch(() => {
+                this.UpdateRequestStatus()
+            })
+    }
+
+    updateStoresCarts = (newStoresCarts) => {
+        this.storesCarts = newStoresCarts
     }
 
     updatedFilteredStoresCarts = (event) => {
@@ -61,73 +90,8 @@ export class Carts {
         this.updateStoresNameList()
     }
 
-    getStoresByProducts = (cart) => {
-        this.loadingStoresSnackBar = true
-        axios.post(`http://localhost:3020/cartPrices`, { cart })
-            .then((response) => {
-                this.handleLoadinStoresSnackBar()
-                this.updateStoresCarts(response.data)
-                this.updatedFilteredStoresCarts()
-                this.calculateTotalPrices(cart)
-            })
-            .catch(() => {
-                this.UpdateRequestStatus()
-            })
-    }
-
-    updateStoresCarts = (newStoresCarts) => {
-        this.storesCarts = newStoresCarts
-    }
-
     handleLoadinStoresSnackBar() {
         this.loadingStoresSnackBar = false
-    }
-
-    UpdateRequestStatus() {
-        this.requsetStoresStatus = false
-    }
-
-    calculateTotalPrices = (productsCart) => {
-
-        let cart = productsCart
-
-        if (this.storesCarts.length !== 0) {
-            this.storesCarts.forEach(store => {
-                Object.keys(store.productCart).forEach(key => {
-                    store.productCart[key]['totalPrice'] = store.productCart[key].initialPrice * cart[key]
-                })
-            })
-        }
-    }
-
-    addItemToShow = (id) => {
-        this.itemToShow = this.storesCartsList.find(item => item.id === id)
-    }
-
-    getFeedBack = (itemName, id) => {
-
-        axios.get(`http://localhost:3020/postsProduct/?productName=${itemName}&&storeId=${id}`)
-            .then((response) => {
-                this.updateFeedBack(response.data)
-            })
-            .catch((error) => alert(error))
-
-    }
-
-    updateFeedBack = (posts) => {
-        this.feedBack = posts
-    }
-
-    sumTotalPriceForStore = (itemsCart) => {
-
-        let sum = 0
-
-        Object.keys(itemsCart).forEach(key => {
-            sum += Math.round(itemsCart[key].totalPrice * 100) / 100
-        })
-
-        return sum
-
     }
 
     updateCitiesNameList() {
@@ -170,5 +134,56 @@ export class Carts {
         let outputArray = Array.from(inputsSet)
         return outputArray
     }
+
+    //revised up to here
+
+    UpdateRequestStatus() {
+        this.requsetStoresStatus = false
+    }
+
+    calculateTotalPrices = (productsCart) => {
+
+        let cart = productsCart
+
+        if (this.storesCarts.length !== 0) {
+            this.storesCarts.forEach(store => {
+                Object.keys(store.productCart).forEach(key => {
+                    store.productCart[key]['totalPrice'] = store.productCart[key].initialPrice * cart[key]
+                })
+            })
+        }
+    }
+
+    addItemToShow = (id) => {
+        this.itemToShow = this.storesCarts.find(item => item.id === id)
+    }
+
+    getFeedBack = (itemName, id) => {
+
+        axios.get(`http://localhost:3020/postsProduct/?productName=${itemName}&&storeId=${id}`)
+            .then((response) => {
+                this.updateFeedBack(response.data)
+            })
+            .catch((error) => alert(error))
+
+    }
+
+    updateFeedBack = (posts) => {
+        this.feedBack = posts
+    }
+
+    sumTotalPriceForStore = (itemsCart) => {
+
+        let sum = 0
+
+        Object.keys(itemsCart).forEach(key => {
+            sum += Math.round(itemsCart[key].totalPrice * 100) / 100
+        })
+
+        return sum
+
+    }
+
+
 
 }
